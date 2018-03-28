fbaaf63cbbe9f0de1ba4aa164c588e60b422103b
Baranov Pavel <pavelbaranow@gmail.com>
2018-03-29 01:06:08 +0300

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,5 +1,6 @@
 //print("Hello World! :-)");
 
+
 extension String {
     subscript (i: Int) -> Character {
         return Array(self.characters)[i]
@@ -12,6 +13,55 @@ extension String {
     subscript (r: CountableRange<Int>) -> String {
         return self[r.lowerBound...r.upperBound-1]
     }
+}
+
+func atoi(str: Character) -> Double 
+{
+	var result: Double = 0;
+	if (str == "1")
+	{
+	    result = 1;
+	}
+	
+		if (str == "2")
+	{
+	    result = 2;
+	}
+	
+		if (str == "3")
+	{
+	    result = 3;
+	}
+	
+		if (str == "4")
+	{
+	    result = 4;
+	}
+	
+		if (str == "5")
+	{
+	    result = 5;
+	}
+	
+		if (str == "6")
+	{
+	    result = 6;
+	}
+	
+		if (str == "7")
+	{
+	    result = 7;
+	}
+		if (str == "8")
+	{
+	    result = 8;
+	}
+		if (str == "9")
+	{
+	    result = 9;
+	}
+	//--
+	return result
 }
 
 class TPostfix
@@ -114,6 +164,101 @@ class TPostfix
          return postfix;
     }
     
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+
+
+    func Calculate() -> Double
+{
+	//https://ru.wikipedia.org/wiki/%D0%9E%D0%B1%D1%80%D0%B0%D1%82%D0%BD%D0%B0%D1%8F_%D0%BF%D0%BE%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F_%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D1%8C#.D0.9E.D0.B1.D1.89.D0.B8.D0.B9_.D0.BF.D0.BE.D1.80.D1.8F.D0.B4.D0.BE.D0.BA  
+	//Алгоритм:
+	//1. Обработка входного символа
+	//    a) Если на вход подан операнд, он помещается на вершину стека.
+	//    b) Если на вход подан знак операции, то соответствующая операция выполняется над требуемым количеством значений, 
+	//    извлечённых из стека, взятых в порядке добавления.Результат выполненной операции кладётся на вершину стека.
+	//2. Если входной набор символов обработан не полностью, перейти к шагу 1.
+	//3. После полной обработки входного набора символов результат вычисления выражения лежит на вершине стека.
+	var a: String = ""
+	var b = [Double]()
+    var flag: Int = 0
+    var k: Double = 0
+	
+    var stack = [Double]()
+
+    for i in 0...postfix.characters.count - 1
+	{
+		//--a--
+		if (postfix[i] != "+" && postfix[i] != "-" && postfix[i] != "*" && postfix[i] != "/")
+		{
+			flag = 0
+			a.insert(postfix[i], at: a.endIndex)
+			for j in 0...i
+			{
+				if (postfix[i] == a[j])
+				{
+					flag = 1
+					stack.append(b[j])
+					break
+				}
+			}
+
+			if (flag == 0)
+			{
+		
+                //--
+                var tmp : Character = postfix[i];
+                
+                //--
+				if ((tmp == "0") || (tmp == "1") || (tmp == "2") || (tmp == "3") || (tmp == "4") || (tmp == "5") || (tmp == "6") || (tmp == "7") || (tmp == "8") || (tmp == "9") )
+				{
+				    b[i] = atoi(str: tmp); // преобразовать символы к типу double
+				}
+				else
+				{
+    				  var k: Double = Double(?readLine())
+    				print(postfix[i], "=");
+    				cin >> k;
+    				b[i] = k;
+				}
+				stack.Push(b[i]);
+			}
+
+		}
+		else
+			//--b--
+		{
+			if (postfix[i] == "+")
+			{
+				var tmp1: Double = stack.Pop();
+				var tmp11: Double = stack.Pop();
+				stack.Push(tmp11 + tmp1);
+			}
+			if (postfix[i] == "-")
+			{
+				var tmp1: Double = stack.Pop();
+				var tmp11: Double = stack.Pop();
+				stack.Push(tmp11 - tmp1);
+			}
+			if (postfix[i] == "/")
+			{
+				var tmp1: Double = stack.Pop();
+				var tmp11: Double = stack.Pop();
+				stack.Push(tmp11 / tmp1);
+			}
+			if (postfix[i] == "*")
+			{
+				var tmp1: Double = stack.Pop();
+				var tmp11: Double = stack.Pop();
+				stack.Push(tmp11 * tmp1);
+			}
+		}
+
+	}
+
+	return stack.Pop();
+}
 
 };
 
