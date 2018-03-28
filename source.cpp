d028ce115bb5edbdd4f9d167f4414481700d5f7c
Baranov Pavel <pavelbaranow@gmail.com>
2018-03-28 13:13:55 +0300

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -33,14 +33,14 @@ class TPostfix
     
     func GetSizeInfix() -> Int  {return lenInfix}
     
-    func Priority(val: String) -> Int
+    func Priority(val: Character) -> Int
 	{
-		if ((val[0] == "*") || (val[0] == "/")) 
+		if ((val == "*") || (val == "/")) 
 		{
 		    return 2;
 		}
 	
-		if ((val[0] == "+") || (val[0] == "-")) 
+		if ((val == "+") || (val == "-")) 
 		{
 		    return 1;
 		}
@@ -63,15 +63,55 @@ class TPostfix
         //--2--
 	    	if (infix[i] == "(")
 	    	{
-	        	stack_op.insert(infix[i], at: stack_op.endIndex) 
+	        	stack_op.insert(infix[i], at: stack_op.endIndex)//Push
 	    	}
 	    //--3--  
 	    if (infix[i] == ")")
 	    {
-
+	    	while (stack_op[stack_op.characters.count - 1] != "(")//Top
+			{
+                result.insert( stack_op[stack_op.characters.count - 1], at: result.endIndex)//Pop1
+                stack_op.remove(at: stack_op.index(before: stack_op.endIndex))//Pop2
+			}
+			stack_op.remove(at: stack_op.index(before: stack_op.endIndex))//Pop2
+	    }
+	    //-----
+	    if ((infix[i] == "+") || (infix[i] == "-") || (infix[i] == "*") || (infix[i] == "/"))
+	    {
+	        //--a--
+			if (stack_op.characters.count == 0 || stack_op[stack_op.characters.count - 1] == "(") 
+			{
+			    stack_op.insert(infix[i], at: stack_op.endIndex)//Push
+			}
+			//--b--
+			else if (Priority(val: infix[i]) > Priority(val: stack_op[stack_op.characters.count - 1]))
+			{
+		    	stack_op.insert(infix[i], at: stack_op.endIndex)//Push
+			}
+			//--c--
+			else
+			{
+				while (stack_op.characters.count != 0 && Priority(val: infix[i]) <= Priority(val: stack_op[stack_op.characters.count - 1]))
+				{
+				result.insert( stack_op[stack_op.characters.count - 1], at: result.endIndex) 
+                                result.insert( stack_op[stack_op.characters.count - 1], at: result.endIndex)//Pop1
+                stack_op.remove(at: stack_op.index(before: stack_op.endIndex))//Pop2
+				}
+				stack_op.insert(infix[i], at: stack_op.endIndex)//Push
+				//-----
+			}
 	    }
          //--
-         }
+        }
+        //--4--
+        while (stack_op.characters.count != 0)//пока
+        {
+            
+            result.insert( stack_op[stack_op.characters.count - 1], at: result.endIndex)//Pop1
+            stack_op.remove(at: stack_op.index(before: stack_op.endIndex))//Pop2
+        }
+        //-----
+         postfix = result;
          return postfix;
     }
     
@@ -97,86 +137,7 @@ print(postfix.GetSizeInfix())
 print("----");
 print("ToPostfix") 
 postfix.ToPostfix()
-
-
-/*
-//print("Hello World! :-)");
-
-class TPostfix
-{
-  var infix: String
-  var postfix: String
-
-  init(s: String) 
-  {
-    self.infix = s
-    self.postfix = s
-  }
-  
-  func GetInfix() -> String 
-  {
-    return infix
-  }
-  
-    func GetPostfix() -> String 
-  {
-    return postfix
-  }
-    
-    func GetSizeInfix() -> Int
-    {
-        return infix.characters.count
-    }
-    
-    func ToPostfix() -> String
-    {
-        var len: Int = GetSizeInfix()
-        var result: String  = ""
-        var stack = [String]()
-       
-        var count: Int = 0
-        var i: Int = 0
-        var flag: Int = 0 
-        
-        while i < len 
-        {
-            let index = infix.index(infix.startIndex, offsetBy: i)
-            let indexInc = infix.index(infix.startIndex, offsetBy: i+1)
-            
-            if (infix[index] != "+" && infix[index] != "-" && infix[index] != "*" && infix[index] != "/" && infix[index] != "(" && infix[index] != ")" )
-            {
-                flag = 0
-                while (infix[index] != "+" && infix[index] != "-" && infix[index] != "*" && infix[index] != "/" && infix[index] != "(" && infix[index] != ")" )
-                {
-                    if ((infix[index] >= "a" && infix[index] <= "z") || (infix[index] >= "A" && infix[index] <= "Z")) 
-                    {
-                        flag = 0;
-                    }
-                    else
-                    {
-                        flag = 1
-                    }
-                    result.insert(infix[indexInc], at: result.endIndex)
-                }
-                if( flag==1)
-                {
-                    result.insert(" ", at: result.endIndex)
-                }
-                
-            }
-            //--
-            i = i + 1
-        }
-        
-        //--
-        return postfix;
-    }
-};
-
-var postfix = TPostfix(s: "22")
-
-print(postfix.GetInfix())
+print("----");
+print("GetPostfix") 
 print(postfix.GetPostfix())
-print(postfix.GetSizeInfix())
-postfix.ToPostfix()
-*/
+print("----");
